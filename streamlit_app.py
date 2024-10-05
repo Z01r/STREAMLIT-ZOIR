22591825e1f51dc81c07dd579fb7019b6d2be5ce
Z01r <140719010+Z01r@users.noreply.github.com>
2024-10-05 19:19:54 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,19 @@ with st.expander('Initial data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+# Create a DataFrame for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+  st.header('Inout Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
+  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
