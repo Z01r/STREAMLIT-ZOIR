a6faea3a29ca39c5579aea415cdb22910ba77be6
Z01r <140719010+Z01r@users.noreply.github.com>
2024-10-05 19:28:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 st.title('ZOIR-STREAMLIT')
 
-st.write('Hello world!')
-
 with st.expander('Initial data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   
@@ -19,7 +17,17 @@ with st.expander('Initial data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
-# Create a DataFrame for the input features
+
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # Create a DataFrame for the input features
   data = {'island': island,
           'bill_length_mm': bill_length_mm,
           'bill_depth_mm': bill_depth_mm,
@@ -28,26 +36,25 @@ with st.expander('Data visualization'):
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
-  st.header('Inout Features')
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
-  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
-  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'female'))
-encode = ['island', 'sex']
-df_penguins = pd.get_dummies (input_penguins, prefix=encode)
 
-X = df_penguins [1:]
-input_row = df_penguins [:1]
+# Data preparation
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
 # Encode y
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 def target_encode(val):
-  return target_mapper [val]
+  return target_mapper[val]
+
 y = y_raw.apply(target_encode)
-with st.expander ('Data preparation'):
+
+with st.expander('Data preparation'):
   st.write('**Encoded X (input penguin)**')
   input_row
   st.write('**Encoded y**')
